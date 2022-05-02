fa9a2163f86e4ebb3fc70ec7fa2c87d35e2ef758
eliottd7 <98353358+eliottd7@users.noreply.github.com>
2022-05-02 11:42:02 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/EC327FinalRepo/MainActivity.java
+++ app/src/main/java/com/example/EC327FinalRepo/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.myapplication_1;
 
+import android.app.AlertDialog;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.Manifest;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import androidx.preference.PreferenceManager;
+import java.util.ArrayList;
+import java.util.HashSet;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.Button;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 static ArrayList<String> notes=new ArrayList<>();
