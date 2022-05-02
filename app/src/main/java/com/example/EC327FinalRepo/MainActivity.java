9faeda66df4599362b227cf6911c85db319ac699
eliottd7 <98353358+eliottd7@users.noreply.github.com>
2022-05-02 11:57:58 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/EC327FinalRepo/MainActivity.java
+++ app/src/main/java/com/example/EC327FinalRepo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication_1;
+package com.example.EC327FinalRepo;
 
 import android.app.AlertDialog;
 import androidx.annotation.NonNull;
@@ -30,10 +30,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final int REQUEST_CALL = 1;
-    static ArrayList<String> notes=new ArrayList<>();
-    static ArrayAdapter arrayAdapter;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,81 +52,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        
-        ListView listview=(ListView)findViewById(R.id.listView);
+
         ImageButton callButton = findViewById(R.id.buttonCal);
         ImageButton settingsButton = findViewById(R.id.buttonSet);
-        SharedPreferences sharedPreferences =getApplicationContext().getSharedPreferences(("com.example.myapplication_1"),Context.MODE_PRIVATE);
-        HashSet<String> set=(HashSet<String>) sharedPreferences.getStringSet("notes",null);
-        if (set==null) {
-            notes.add("Please provide the date, indicate type of injury, and pain rating from 1-10");
 
-        }else {
-
-            notes = new ArrayList<>(set);
-        }
-
-        arrayAdapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,notes);
-        listview.setAdapter(arrayAdapter);
-
-        notes.add("Please provide the date, indicate type of injury, and pain rating from 1-10");
-
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getApplicationContext(),Note_Editor.class);
-                intent.putExtra("noteId",i);
-                startActivity(intent);
-            }
-        });
-        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                final int itemToDelete=i;
-
-                new AlertDialog.Builder(MainActivity.this)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setTitle("Are you sure you want to delete")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                               notes.remove(itemToDelete);
-                               arrayAdapter.notifyDataSetChanged();
-                                SharedPreferences sharedPreferences =getApplicationContext().getSharedPreferences(("com.example.myapplication_1"), Context.MODE_PRIVATE);
-
-                                HashSet<String> set=new HashSet<>(MainActivity.notes);
-                                sharedPreferences.edit().putStringSet("notes",set).apply();
-
-                            }
-                        })
-                        .setNegativeButton("No",null)
-                        .show();
-                return true;
-
-            }
-        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater=getMenuInflater();
-        menuInflater.inflate(R.menu.add_note_item,menu);
-        return super.onCreateOptionsMenu(menu);
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if(item.getItemId()==R.id.addNote){
-            Intent intent=new Intent(getApplicationContext(),Note_Editor.class);
-            startActivity(intent);
-        }
-        return super.onOptionsItemSelected(item);
-
-    }
-    callButton.setOnClickListener(new View.OnClickListener(){
+        callButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 makePhoneCall();
@@ -144,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void makePhoneCall() {
 
+
        //String defaultValue = getResources().getString(R.string.prefs_change_number);
         String number;
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -158,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
             String dial = "tel:" + number;
             startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
             System.out.println("6");
+
         }
     }
 
@@ -174,6 +105,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
-
-
