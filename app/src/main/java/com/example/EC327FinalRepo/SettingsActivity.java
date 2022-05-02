69e83ca49e0cb0d9903de4dc50277bb7472c8762
eliottd7 <98353358+eliottd7@users.noreply.github.com>
2022-05-01 22:02:10 -0400

    Add files via upload

--- app/src/main/java/com/example/EC327FinalRepo/SettingsActivity.java
+++ app/src/main/java/com/example/EC327FinalRepo/SettingsActivity.java
@@ -4,31 +4,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-/* public class SettingsActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.buttonSet, new SettingsFragment())
-                    .commit();
-        }
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
-    }
-} */
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
