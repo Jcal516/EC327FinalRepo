9e5fb9a06738b034a56460348ddf28d39c94b2ca
eliottd7 <98353358+eliottd7@users.noreply.github.com>
2022-05-04 11:43:25 -0400

    Add files via upload

--- app/src/main/java/com/example/EC327FinalRepo/Note_Editor.java
+++ app/src/main/java/com/example/EC327FinalRepo/Note_Editor.java
@@ -1,7 +1,6 @@
 package com.example.EC327FinalRepo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,10 +8,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
-
 import java.util.HashSet;
 
 public class Note_Editor extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +22,13 @@ public class Note_Editor extends AppCompatActivity {
         Intent intent=getIntent();
         int noteId = intent.getIntExtra("noteId", -1);
         if (noteId !=-1){
-            editText.setText(MainActivity.notes.get(noteId));
+            editText.setText(Notes_Activity.notes.get(noteId));
 
         }
         else{
-            MainActivity.notes.add("");
-            noteId=MainActivity.notes.size()-1;
-            MainActivity.arrayAdapter.notifyDataSetChanged();
+            Notes_Activity.notes.add("");
+            noteId=Notes_Activity.notes.size()-1;
+            Notes_Activity.arrayAdapter.notifyDataSetChanged();
         }
 
         int finalNoteId = noteId;
@@ -41,12 +40,12 @@ public class Note_Editor extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-             MainActivity.notes.set(finalNoteId,String.valueOf(charSequence));
-             MainActivity.arrayAdapter.notifyDataSetChanged();
+                Notes_Activity.notes.set(finalNoteId,String.valueOf(charSequence));
+                Notes_Activity.arrayAdapter.notifyDataSetChanged();
 
                 SharedPreferences sharedPreferences =getApplicationContext().getSharedPreferences(("com.example.myapplication_1"), Context.MODE_PRIVATE);
 
-                HashSet<String> set=new HashSet<>(MainActivity.notes);
+                HashSet<String> set=new HashSet<>(Notes_Activity.notes);
                 sharedPreferences.edit().putStringSet("notes",set).apply();
 
 
